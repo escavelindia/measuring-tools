ebf40510e83fe603b1d82afd6b29724d409228ea
Ankush Taneja <ankush.taneja@escavel.in>
2020-07-17 22:27:07 +0530

    - android platform switched - mouseMovement script changes - Scene enlarged changes

--- Assets/Modules/Physics/MeasuringTools/Scripts/MouseMovement.cs
+++ Assets/Modules/Physics/MeasuringTools/Scripts/MouseMovement.cs
@@ -20,6 +20,7 @@ namespace MeasuringTools
         public float yMaxLimit = 90f;
 
         public float lableShowLimit = 25f;
+        Camera cam;
 
         Vector3 lastPosition;
         Vector3 negDistance;
@@ -34,6 +35,19 @@ namespace MeasuringTools
 
         public bool isScrolling;
         public bool isOrbit;
+        // mobile variables
+        Vector2 firstpoint;
+        private Vector2 secondpoint;
+        private float xAngle; //angle for axes x for rotation
+        private float yAngle;
+        private float xAngTemp; //temp variable for angle
+        private float yAngTemp;
+        float TouchZoomSpeed = 0.1f;
+        public GameObject mobileControlMessage;
+        public GameObject sliderZoom;
+
+        float ZoomMinBound = 20f;
+        float ZoomMaxBound = 70f;
 
         private void Awake()
         {
@@ -42,12 +56,112 @@ namespace MeasuringTools
 
         void Start()
         {
+            cam = Camera.main;
+
             Vector3 angles = transform.eulerAngles;
             x = angles.y;
             y = angles.x;
             negDistance = new Vector3(0f, 0f, -distance);
+            if (Input.touchSupported)
+            {
+                xAngle = 0.0f;
+                yAngle = 0.0f;
+                transform.rotation = Quaternion.Euler(yAngle, xAngle, 0.0f);
+                xSpeed = 60f;
+                ySpeed = 60f;
+                if (PlayerPrefs.GetInt("seenMobileInputInfo") == 0)
+                {
+                    mobileControlMessage.SetActive(true);
+                    PlayerPrefs.SetInt("seenMobileInputInfo", 1);
+                }
+            }
+        }
+        void TouchZoom()
+        {
+            // Pinch to zoom
+            if (Input.touchCount >= 2)
+            {
+                isScrolling = true;
+                // get current touch positions
+                Touch tZero = Input.GetTouch(0);
+                Touch tOne = Input.GetTouch(1);
+                // get touch position from the previous frame
+                Vector2 tZeroPrevious = tZero.position - tZero.deltaPosition;
+                Vector2 tOnePrevious = tOne.position - tOne.deltaPosition;
+
+                float oldTouchDistance = Vector2.Distance(tZeroPrevious, tOnePrevious);
+                float currentTouchDistance = Vector2.Distance(tZero.position, tOne.position);
+
+                // get offset value
+                float deltaDistance = oldTouchDistance - currentTouchDistance;
+                Zoom(deltaDistance, TouchZoomSpeed);
+
+                // different 
+                if (InstructionMeasuringTools.instance.isInstructionClick && isScrolling)
+                {
+
+                    InstructionMeasuringTools.instance.cameraZoomI.SetActive(false);
+                    InstructionMeasuringTools.instance.cameraOrbitI.SetActive(true);
+                }
+            }
+            else if (target && Input.touchCount == 1/* && Input.GetTouch(0).position.x > Screen.width / 2*/ && Input.GetTouch(0).phase == TouchPhase.Moved)
+            {
+                Orbit(Input.GetTouch(0));
+
+            }
+
+            else if (Input.GetTouch(0).phase == TouchPhase.Ended && Input.touchCount == 1)
+            {
+                if (InstructionMeasuringTools.instance.isInstructionClick && !InstructionMeasuringTools.instance.isScrolligInstruction)
+                {
+
+                    Debug.Log("hh");
+                    InstructionMeasuringTools.instance.cameraOrbitI.SetActive(false);
+                    if (isScrolling == true)
+                    {
+                        isOrbit = true;
+                        isScrolling = false;
+                        InstructionMeasuringTools.instance.cameraOrbitI.SetActive(false);
+                        CameraSwitchVernierCaliper.instance.mainCam.GetComponent<MouseMovement>().enabled = false;
+                        UIManager.instance.showLabelB.GetComponent<Button>().interactable = true;
+                        InstructionMeasuringTools.instance.showLablesI.SetActive(true);
+                        CameraSwitchVernierCaliper.instance.FrontCameraView();
+                        Camera.main.fieldOfView = 70f;
+                    }
+                }
+            }
+          
+        }
+        void Orbit(Touch touch)
+        {
+            if (!MouseCursor.obstacle)
+            {
+                x += touch.deltaPosition.x * xSpeed * 0.01f /* * distance*/;
+                y -= touch.deltaPosition.y * ySpeed * 0.01f /* * distance*/;
+                y = ClampAngle(y, yMinLimit, yMaxLimit);
+
+                Quaternion rotation = Quaternion.Euler(y, x, 0);
+
+                //distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel") * 5, distanceMin, distanceMax);
+
+                //RaycastHit hit;
+
+                //if (Physics.Linecast(target.position, transform.position, out hit))
+                //{
+                //}
+                Vector3 negDistance = new Vector3(0.0f, 0.0f, -distance);
+                Vector3 position = rotation * negDistance + target.position;
+                transform.rotation = rotation;
+                transform.position = position;
+            }
         }
 
+        void Zoom(float deltaMagnitudeDiff, float speed)
+        {
+            cam.fieldOfView += deltaMagnitudeDiff * speed;
+            // set min and max value of Clamp function upon your requirement
+            cam.fieldOfView = Mathf.Clamp(cam.fieldOfView, ZoomMinBound, ZoomMaxBound);
+        }
         void LateUpdate()
         {
 
